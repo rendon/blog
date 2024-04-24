07dce840e66d73bfee9d47db0dcbf0c050627602
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2024-04-23 20:02:57 -0700

    Print logs to the standard output and error
    This will allow us to see the logs with docker logs

--- unicorn.rb
+++ unicorn.rb
@@ -8,12 +8,6 @@ working_directory ENV['BLOG_PATH'].to_s
 # pid "/path/to/pids/unicorn.pid"
 pid "#{ENV['BLOG_PATH']}/pids/unicorn.pid"
 
-# Path to logs
-# stderr_path "/path/to/logs/unicorn.log"
-# stdout_path "/path/to/logs/unicorn.log"
-stderr_path "#{ENV['BLOG_PATH']}/logs/unicorn.log"
-stdout_path "#{ENV['BLOG_PATH']}/logs/unicorn.log"
-
 # Unicorn socket
 # listen "/tmp/unicorn.[app name].sock"
 listen '/tmp/unicorn.blog.sock'
